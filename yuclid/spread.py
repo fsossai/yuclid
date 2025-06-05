d9f163ae6007bd705f3aa7c0ab427d6dfef10a76
fsossai <federico.sossai@gmail.com>
2025-06-05 00:40:12 -0500

    Fixed `spread.py`

--- yuclid/spread.py
+++ yuclid/spread.py
@@ -4,14 +4,14 @@ import pandas as pd
 import numpy as np
 import re
 
-available = {
-    "sd", "piX", "rsdX", "mad", "range", "iqr"
-}
+available = {"sd", "piX", "rsdX", "mad", "range", "iqr"}
+
 
 def mad(y):
     if not isinstance(y, np.ndarray):
         y = np.array(y)
     return np.median(np.abs(y - np.median(y)))
+
 
 def lower(spread_measure):
     n = re.search(r"\d+(\.\d+)?", spread_measure)
@@ -20,7 +20,7 @@ def lower(spread_measure):
         return lambda y: y.mean() - coeff * y.std()
     elif spread_measure.startswith("pi"):
         p = float(n.group()) / 100.0
-        return lambda y: np.quantile(y, (1 - p)/2)
+        return lambda y: np.quantile(y, (1 - p) / 2)
     elif spread_measure.startswith("rsd"):
         coeff = float(n.group())
         return lambda y: y.mean() - coeff * (1 / norm.ppf(0.75)) * mad(y)
@@ -31,6 +31,7 @@ def lower(spread_measure):
     elif spread_measure == "iqr":
         return lambda y: np.quantile(y, 0.25)
 
+
 def upper(spread_measure):
     n = re.search(r"\d+(\.\d+)?", spread_measure)
     if spread_measure.startswith("sd"):
@@ -38,7 +39,7 @@ def upper(spread_measure):
         return lambda y: y.mean() + coeff * y.std()
     elif spread_measure.startswith("pi"):
         p = float(n.group()) / 100.0
-        return lambda y: np.quantile(y, (1 + p)/2)
+        return lambda y: np.quantile(y, (1 + p) / 2)
     elif spread_measure.startswith("rsd"):
         coeff = float(n.group())
         return lambda y: y.mean() + coeff * (1 / norm.ppf(0.75)) * mad(y)
@@ -49,14 +50,13 @@ def upper(spread_measure):
     elif spread_measure == "iqr":
         return lambda y: np.quantile(y, 0.75)
 
-def draw(ax, spread_measures, df, x, y, z=None, colors=None, palette=None, style="area"):
-    if z is None:
-        z = "z" * (max([len(c) for c in columns]) + 1) # unique column name
-        df[z] = 0
 
+def draw(
+    ax, spread_measures, df, x, y, z=None, colors=None, palette=None, style="area"
+):
     alphas = {
         "area": np.linspace(0.15, 0.05, len(spread_measures)),
-        "bar": np.linspace(0.30, 0.10, len(spread_measures))
+        "bar": np.linspace(0.30, 0.10, len(spread_measures)),
     }[style]
 
     x_dom = df[x].unique()
@@ -81,8 +81,20 @@ def draw(ax, spread_measures, df, x, y, z=None, colors=None, palette=None, style
             y_lower = ys_lower.xs(z_val)
             y_upper = ys_upper.xs(z_val)
             if style == "area":
-                ax.fill_between(x_dom, y_lower, y_upper,
-                                interpolate=True, color=color, alpha=alphas[i])
+                ax.fill_between(
+                    x_dom,
+                    y_lower,
+                    y_upper,
+                    interpolate=True,
+                    color=color,
+                    alpha=alphas[i],
+                )
             elif style == "bar":
-                ax.vlines(x=x, ymin=y_lower, ymax=y_upper,
-                          color=color, alpha=alphas[i], linewidth=4)
+                ax.vlines(
+                    x=x,
+                    ymin=y_lower,
+                    ymax=y_upper,
+                    color=color,
+                    alpha=alphas[i],
+                    linewidth=4,
+                )
